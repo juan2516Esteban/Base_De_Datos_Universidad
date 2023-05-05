b445c4b9b3755676b12ad407cf053eb2bdc8c892
Santiago Orjuela <santiago.orjuela@utp.edu.co>
2023-05-05 15:32:29 -0500

    Mis tablas
    Se agregaron facultades, municipios, discapacidades y dias.

--- Universidad_SQL.sql
+++ Universidad_SQL.sql
@@ -20,6 +20,111 @@ insert into rol_administrativo (Nombre) values('Secretario Academico'), ('Gerent
 select * from rol_administrativo
 
 
+--facultad, dia, discapacidad, municipios, ORJUELA
+
+------------------------------FACULTADES-----------------------------
+CREATE TABLE Facultades
+(
+	id_facultad serial,
+	nombre_facultad varchar(50)
+);
+
+insert into Facultades(nombre_facultad)
+values('Ingenierias'),
+('Ciencias exactas'),
+('Negocios');
+
+alter table Facultades
+add primary key (id_facultad);
+
+select * from Facultades;
+
+-------------------------------dias---------------------------------------------
+CREATE TABLE Dias
+(
+	id_dia SERIAL,
+	nombre_dia varchar(20)
+);
+
+insert into Dias(nombre_dia)
+values('Lunes'),
+('Martes'),
+('Miercoles'),
+('Jueves'),
+('Viernes');
+
+
+alter table Dias
+add primary key (id_dia);
+
+select * from Dias
+
+Alter table Dias RENAME id_dias TO id_dia;
+
+-------------------------------discapacidad---------------------------------------------
+Create table Discapacidades
+(
+	id_discapacidad serial,
+	tipo_discapacidad varchar(30)
+)
+
+INSERT INTO Discapacidades(tipo_discapacidad)
+VALUES('mental'),
+      ('intelectual'),
+      ('psicologica'),
+      ('multiple'),
+      ('sensorial'),
+      ('auditiva'),
+      ('fisica'),
+      ('visual'),
+      ('ninguna');
+	  
+	  
+alter table Discapacidades
+add primary key (id_discapacidad);
+	  
+select * from Discapacidades;
+
+-------------------------------Municipios---------------------------------------------
+CREATE TABLE Municipios (
+  id_municipio serial PRIMARY KEY,
+  municipio VARCHAR(50),
+  departamento VARCHAR(50)
+);
+
+INSERT INTO Municipios (municipio, departamento) VALUES
+  ('Armenia', 'Quindio'),
+  ( 'Cordoba', 'Quindio'),
+  ( 'Tebaida', 'Quindio'),
+  ( 'Calarca', 'Quindio'),
+  ( 'Pijao', 'Quindio'),
+  ( 'Genova', 'Quindio'),
+  ( 'Buenavista', 'Quindio'),
+  ( 'Montenegro', 'Quindio'),
+  ( 'Filandia', 'Quindio'),
+  ( 'Circasia', 'Quindio'),
+  ( 'Salento', 'Quindio'),
+  ( 'Quimbaya', 'Quindio'),
+  ( 'Caicedonia', 'Quindio'),
+  ( 'Pereira', 'Risaralda'),
+  ( 'Santa rosa', 'Risaralda'),
+  ( 'Mistrató', 'Risaralda'),
+  ( 'Santuario', 'Risaralda'),
+  ( 'Dosquebradas', 'Risaralda'),
+  ( 'La celia', 'Risaralda'),
+  ( 'Pueblo rico', 'Risaralda'),
+  ( 'Chinchina', 'Risaralda'),
+  ( 'Guática', 'Risaralda'),
+  ( 'Balboa', 'Risaralda'),
+  ( 'Apia', 'Risaralda'),
+  ( 'Belén de umbría', 'Risaralda'),
+  ( 'Belalcazar', 'Risaralda'),
+  ( 'La virginia', 'Risaralda');
+  
+  select * from Municipios
+  --pruebitaaaa
+
+
 
 
 
