a707972003f96d1048fc128aa5cd5d0bbebafda0
Juan Esteban Salazar <juanesteban.salazar1@utp.edu.co>
2023-05-05 21:27:00 -0500

    Second commit

--- Universidad_SQL.sql
+++ Universidad_SQL.sql
@@ -124,7 +124,43 @@ INSERT INTO Municipios (municipio, departamento) VALUES
   select * from Municipios
   --pruebitaaaa
 
+----------------------------------------Fabian-------------------------------------------------
 
+CREATE TABLE victimas(
+id_victimas int NOT NULL AUTO_INCREMENT,
+    PRIMARY KEY(id_victimas),
+nombre_victimas varchar(150) NOT NULL
+)
+INSERT INTO `victimas`(`id_victimas`, `nombre_victimas`) VALUES ('1','poblacion victimas del conflicto'), ('2','poblacion con discapacidad'),('3','poblacion adulta mayor'),('4','genero'),('5','poblacion situacion de calle'),('6','poblacion etnica'),('7','poblacion migrante'),('8','ninguno')
+
+CREATE TABLE bono_alimenticio(
+id_bono_alimenticio int NOT NULL AUTO_INCREMENT,
+    PRIMARY KEY(id_bono_alimenticio),
+valor_bono int NOT NULL
+)
+INSERT INTO `bono_alimenticio`(`id_bono_alimenticio`, `valor_bono`) VALUES ('1','3000'),('2','5000'),('3','8000'),('4','0')
+
+CREATE TABLE asignatura(
+id_asignatura int NOT NULL AUTO_INCREMENT,
+    PRIMARY KEY(id_asignatura),
+nombre_asignatura varchar(150) NOT NULL
+)
+INSERT INTO `asignatura`(`id_asignatura`, `nombre_asignatura`) VALUES ('1','calculo i'),('2','calculo ii'),('3','calculo iii'),('4','calculo iv'),('5','programacion i'),('6','programacion ii'),('7','programacion iii'),('8','programacion iv'),('9','estructura de datos '),('10','patrones de diseño'),('11','entornos de desarrollo de software'),('12','estadistica '),('13','bases de datos'),('14','programacion web'),('15','programacion movil')
+
+CREATE TABLE carrera(
+id_carrera int NOT NULL AUTO_INCREMENT,
+    PRIMARY KEY(id_carrera),
+nombre_carrera varchar(150) NOT NULL
+)
+INSERT INTO `carrera`(`id_carrera`, `nombre_carrera`) VALUES ('1','ingenieria electrica'),('2','ingeniera mecatronica'),('3','ingenieria de sistemas '),('4','ingenieria industrial'),('5','ingenieria biomedica'),('6','ingenieria aeronautica'),('7','ingeniera quimica '),('8','ingeniera fisica'),('9','ingenieria mecanica '),('10','ingenieria electronica'),('11','ingeniera agricola '),('12','ingeniera de maderas'),('13','ingenieria agroindustrial'),('14','ingenieria civil '),('15','ingeneria Software'),('16','licenciatura en matematicas'),('17','licenciatura en estadistica'),('18','licenciatura en astronomia'),('19','contabilidad'),('20','administracion de empresas'),('21','marketing '),('22','negocios internacionales')
+
+CREATE TABLE grupo_etnico(
+id_grupo_etnico int NOT NULL AUTO_INCREMENT,
+    PRIMARY KEY(id_grupo_etnico),
+nombre_etnico varchar(150) NOT NULL
+)
+INSERT INTO `grupo_etnico`(`id_grupo_etnico`, `nombre_etnico`) VALUES ('1','indigena
+'),('2','afrocolombi@'),('3','raizal'),('4','palenquero'),('5','gitano'),('6','ninguno')
 
 
 
